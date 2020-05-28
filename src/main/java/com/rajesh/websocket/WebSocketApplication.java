16ea8dafc684f3458e67589c965d9b6ddb719e94
rajeshwar.y <rajeshwar.y@DIPL-RE-LAP427.diplit.local>
2020-05-28 11:25:40 +0530

    first commit

--- src/main/java/com/rajesh/websocket/WebSocketApplication.java
+++ src/main/java/com/rajesh/websocket/WebSocketApplication.java
@@ -3,7 +3,6 @@ package com.rajesh.websocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class WebSocketApplication {
 
